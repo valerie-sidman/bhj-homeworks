c7420dae46fcae8cc019f6523d3f0e6878aa4d6b
Valerie Sidman <valerie.sidman@gmail.com>
2020-07-28 13:50:33 +0300

    popups is done

---
+++ element-search/popups/task.js
@@ -0,0 +1,14 @@
+const modalMainList = document.querySelector('#modal_main');
+modalMainList.classList.add('modal_active');
+
+const modalSuccessList = document.querySelector('#modal_success');
+modalMainList.querySelector('.show-success').onclick = () => {
+  modalSuccessList.classList.add('modal_active');
+}
+
+const modalList = document.querySelectorAll('.modal');
+for (let i = 0; i < modalList.length; i++) {
+  modalList[i].querySelector('.modal__close').onclick = () => {
+    modalList[i].classList.remove('modal_active');
+  }
+}
