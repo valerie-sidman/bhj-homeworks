0749fb6b88611772fda0122e8787197a87495a67
Valerie Sidman <valerie.sidman@gmail.com>
2020-08-17 12:21:30 +0300

    keysolo is done

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+     document.addEventListener('keyup', (event) => {
+       if (event.key.toLowerCase() == this.currentSymbol.textContent.toLowerCase()) {
+         this.success();
+        } else {
+         this.fail();
+        }
+     })
   }
 
   success() {
@@ -87,4 +87,3 @@ class Game {
 }
 
 new Game(document.getElementById('game'))
-
