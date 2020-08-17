7bfb50fa4e5dcb0e2f074e356975514e7afa6a3f
Valerie Sidman <valerie.sidman@gmail.com>
2020-08-17 13:26:32 +0300

    menu is fixed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,7 +3,10 @@ for (let i = 0; i < menuLinkList.length; i++) {
   menuLinkList[i].onclick = () => {
     const childMenu = menuLinkList[i].parentElement.querySelector('.menu');
     if (childMenu) {
-
+      if (childMenu.classList.contains('menu_active')) {
+        childMenu.classList.remove('menu_active');
+        return false;
+      }
       const menuMain = menuLinkList[i].closest('.menu_main');
       const menuList = menuMain.querySelectorAll('.menu');
       for (let i = 0; i < menuList.length; i++) {
