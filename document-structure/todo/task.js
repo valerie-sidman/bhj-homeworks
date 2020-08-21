a3deea7d3ac92425ad8e40559daedf390927b5b9
Valerie Sidman <valerie.sidman@gmail.com>
2020-08-21 15:21:10 +0300

    todo fixed

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,21 +1,30 @@
 const taskInput = document.querySelector('.tasks__input');
 const tasksList = document.querySelector('.tasks__list');
-taskInput.addEventListener('keydown', (event) => {
-  if (event.keyCode == 13) {
-    if (taskInput.value != '') {
-      tasksList.insertAdjacentHTML('beforeEnd',
-      `<div class="task">
-        <div class="task__title">
-          ${taskInput.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-      </div>`);
-      const currentTask = tasksList.lastChild;
-      currentTask.querySelector('.task__remove').addEventListener('click', () => {
-        currentTask.remove();
-      });
-      taskInput.value = '';
-    }
-    event.preventDefault();
+const tasksAddBtn = document.querySelector('.tasks__add');
+function addTask() {
+  if (taskInput.value != '') {
+    tasksList.insertAdjacentHTML('beforeEnd',
+    `<div class="task">
+      <div class="task__title">
+        ${taskInput.value}
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+    </div>`);
+    const currentTask = tasksList.lastChild;
+    currentTask.querySelector('.task__remove').addEventListener('click', () => {
+      currentTask.remove();
+    });
+    taskInput.value = '';
+  }
+}
+
+tasksAddBtn.addEventListener('click', (e) => {
+  addTask();
+  e.preventDefault();
+})
+taskInput.addEventListener('keydown', (e) => {
+  if (e.keyCode == 13) {
+    addTask();
+    e.preventDefault();
   }
 })
